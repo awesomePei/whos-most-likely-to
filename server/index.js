90543c4493ffb4942d8316e0ac8f97b8d38c5831
awesomePei <momotsai910@gmail.com>
2025-06-25 15:02:07 +0800

    Alter server for deployment

--- server/index.js
+++ server/index.js
@@ -183,7 +183,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Socket.IO server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
